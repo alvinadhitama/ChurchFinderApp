dc86bf2e7029f436f686fa30f36f1cf1a4bdfcca
alvinadhitama <alvinadhitama@gmail.com>
2020-02-15 20:26:48 +0700

    update google login color blue

--- app/src/main/java/com/alvin/churchfinderapp/SignInActivity.kt
+++ app/src/main/java/com/alvin/churchfinderapp/SignInActivity.kt
@@ -5,10 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import java.util.*
 
 class SignInActivity : AppCompatActivity() {
+    var googleSignInClient: GoogleSignInClient? = null
+    val RC_SIGN_IN = 1000
+
+    lateinit var uName:String
+    lateinit var uUsername:String
+    lateinit var uEmail:String
+    lateinit var uPhoto:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,13 +57,73 @@ class SignInActivity : AppCompatActivity() {
                         Toast.makeText(this,"Email or Password is wrong",Toast.LENGTH_SHORT).show()
                     }
             }
-
-
         }
 
         btn_register.setOnClickListener {
             val intent =  Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        btn_login_google.setOnClickListener {
+            var signInIntent = googleSignInClient?.signInIntent
+            startActivityForResult(signInIntent,RC_SIGN_IN)
+        }
+    }
+
+    fun firebaseAuthWithGoogle(acct: GoogleSignInAccount?){
+        if (acct != null) {
+            Log.d("SignInActivity","Account: " +acct.id)
+            Log.d("SignInActivity","Name: " +acct.displayName)
+            Log.d("SignInActivity","Email: " +acct.email)
+        }
+        var credential =  GoogleAuthProvider.getCredential(acct?.idToken,null)
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener {
+                task ->
+                if (task.isSuccessful){
+                    Log.d("SignInActivity","Sign In With Google success")
+
+                    val uid = FirebaseAuth.getInstance().uid ?:""
+                    val db = FirebaseFirestore.getInstance()
+                    //uid = acct?.id.toString()
+                    uName = acct?.displayName.toString()
+                    uUsername = acct?.email.toString()
+                    uEmail = acct?.email.toString()
+                    uPhoto = acct?.photoUrl.toString()
+
+                    val user = User(
+                        uid,
+                        uName,
+                        uUsername,
+                        uEmail,
+                        uPhoto
+                    )
+
+                    db.collection("users").document(uid).set(user)
+                        .addOnSuccessListener {
+                            Log.d("SignInActivity","Successfully uploaded user data")
+                        }
+
+                    val intent = Intent(this,HomeActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            var task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            var account = task.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account)
         }
     }
 }
