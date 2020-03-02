39054c68b3fcb1a4b86b1db80170ec564c709f7d
alvinadhitama <alvinadhitama@gmail.com>
2020-03-02 20:34:09 +0700

    add edit profile activity

--- app/src/main/java/com/alvin/churchfinderapp/fragment/ProfileFragment.kt
+++ app/src/main/java/com/alvin/churchfinderapp/fragment/ProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.alvin.churchfinderapp.fragment
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.alvin.churchfinderapp.EditProfileActivity
 import com.alvin.churchfinderapp.R
 import com.alvin.churchfinderapp.utils.Preferences
 import com.bumptech.glide.Glide
@@ -68,8 +70,9 @@ class ProfileFragment : Fragment() {
             .apply(RequestOptions.circleCropTransform())
             .into(iv_profile)
 
-
-        //////////
+        tv_edit_profile.setOnClickListener {
+            startActivity(Intent(activity,EditProfileActivity::class.java))
+        }
 
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
